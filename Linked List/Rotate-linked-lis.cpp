e7a6addd98483e9bf8fe51dde838abdd6ac0e0d6
Divyanshu7001 <divyanshuverma195@gmail.com>
2025-04-29 00:00:26 +0530

    Linked List

---
+++ Linked List/Rotate-linked-lis.cpp
@@ -0,0 +1,125 @@
+/*
+ *
+ * [61] Rotate List
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// Simple but time limit error
+// class Solution
+// {
+// public:
+//     ListNode *rotateRight(ListNode *head, int k)
+//     {
+
+//         if (!head || !head->next)
+//         {
+//             return head;
+//         }
+//         int count = 0;
+//         while (count < k)
+//         {
+//             ListNode *dh = head;
+//             ListNode *temp;
+//             while (dh->next->next != nullptr)
+//             {
+//                 dh = dh->next;
+//             }
+
+//             temp=dh->next;
+//             dh->next=nullptr;
+//             temp->next=head;
+//             head=temp;
+//             count++;
+//         }
+//         return head;
+//     }
+// };
+
+// Find length, reduce k, go while loop and do like before
+//  class Solution
+//  {
+//  public:
+//      ListNode *rotateRight(ListNode *head, int k)
+//      {
+//          if (!head || !head->next)
+//              return head;
+
+//         //Get the length first
+//         int n = 1;
+//         ListNode *temp1 = head;
+//         while (temp1->next != nullptr)
+//         {
+//             n++;
+//             temp1 = temp1->next;
+//         }
+
+//         //Mod to reduce the size
+//         //Logic: a list becomes same after rotated n times
+//         k = k % n;
+//         if (k == 0)
+//             return head;
+
+//         int count = 0;
+//         while (count < k)
+//         {
+//             ListNode *dh = head;
+//             ListNode *temp;
+//             while (dh->next->next != nullptr)
+//             {
+//                 dh = dh->next;
+//             }
+
+//             temp = dh->next;
+//             dh->next = nullptr;
+//             temp->next = head;
+//             head = temp;
+//             count++;
+//         }
+//         return head;
+//     }
+// };
+
+// Find length, reduce k, make the list circular,
+// Get new tail by traveling n-k nodes
+// point the head to newtail->next
+// point newTail->next to nullptr
+class Solution
+{
+public:
+    ListNode *rotateRight(ListNode *head, int k)
+    {
+        if (!head || !head->next)
+            return head;
+
+        int n = 1;
+        ListNode *tail = head;
+        while (tail->next != nullptr)
+        {
+            n++;
+            tail = tail->next;
+        }
+
+        k = k % n;
+        if (k == 0)
+            return head;
+        tail->next = head;
+        for (int i = 0; i < n - k; i++)
+        {
+            tail = tail->next;
+        }
+        head = tail->next;
+        tail->next = nullptr;
+        return head;
+    }
+};
+
