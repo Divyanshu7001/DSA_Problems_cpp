27953a94b27d61a5640e36810b643369c7adefaf
Divyanshu Kumar Verma <153366809+Divyanshu7001@users.noreply.github.com>
2025-05-19 17:29:29 +0530

    3321-TYPE-OF-TRIANGLE Stats: Time: 0 ms (100%), Space: 23 MB (28.31%) - LeetHub

--- 3321-type-of-triangle/3321-type-of-triangle.cpp
+++ 3321-type-of-triangle/3321-type-of-triangle.cpp
@@ -1,24 +1,40 @@
+// Map Approach of adding elements and checking size
+// class Solution {
+// public:
+//     string triangleType(vector<int>& nums) {
+//         if (nums[0] + nums[1] > nums[2] && nums[0] + nums[2] > nums[1] &&
+//             nums[1] + nums[2] > nums[0]) {
+//             unordered_map<int, int> store;
+//             for (int i = 0; i < nums.size(); i++) {
+//                 if (store.find(nums[i])==store.end()){
+//                     store[nums[i]] = store[nums[i]] + 1;       
+//                 }
+//             }
+//             if(store.size()==1){
+//                 return "equilateral";
+//             }
+//             else if(store.size()==2){
+//                 return "isosceles";
+//             }
+//             else 
+//                 return "scalene";
+//         } else {
+//             return "none";
+//         }
+//     }
+// };
+
+//Just the simple approach
 class Solution {
 public:
     string triangleType(vector<int>& nums) {
-        if (nums[0] + nums[1] > nums[2] && nums[0] + nums[2] > nums[1] &&
-            nums[1] + nums[2] > nums[0]) {
-            unordered_map<int, int> store;
-            for (int i = 0; i < nums.size(); i++) {
-                if (store.find(nums[i])==store.end()){
-                    store[nums[i]] = store[nums[i]] + 1;       
-                }
-            }
-            if(store.size()==1){
-                return "equilateral";
-            }
-            else if(store.size()==2){
-                return "isosceles";
-            }
-            else 
-                return "scalene";
-        } else {
-            return "none";
+        int a = nums[0], b = nums[1], c = nums[2];
+
+        if(a + b > c && b + c > a && a + c > b) {
+            if(a == b && b == c && c == a) return "equilateral";
+            else if((a == b) || (b == c) || (c == a)) return "isosceles";
+            return "scalene";
         }
+        return "none";
     }
 };
