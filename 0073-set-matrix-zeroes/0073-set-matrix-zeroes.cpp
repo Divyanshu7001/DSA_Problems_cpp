f193fa25a9de17259741f3c6fa75d3757041ce6e
Divyanshu Kumar Verma <153366809+Divyanshu7001@users.noreply.github.com>
2025-05-21 13:34:56 +0530

    0073-SET-MATRIX-ZEROES Stats: Time: 3 ms (11.48%), Space: 19 MB (6.61%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -6,16 +6,8 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-
+        int n = matrix.size(),m = matrix[0].size();
         vector<vector<int>> matrix2=matrix;;
-
-        // for (int i = 0; i < m; i++) {
-        //     for (int j = 0; j < n; j++) {
-        //         matrix2[i][j] = matrix[i][j];
-        //     }
-        // }
 
         // update the matrix
         for (int i = 0; i < n; i++) {
