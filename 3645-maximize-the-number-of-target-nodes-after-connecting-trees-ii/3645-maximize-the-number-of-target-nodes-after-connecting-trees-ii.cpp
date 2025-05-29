ec802f1cfefd15f753ae835b4608fade906801b5
Divyanshu Kumar Verma <153366809+Divyanshu7001@users.noreply.github.com>
2025-05-29 21:15:26 +0530

    3645-MAXIMIZE-THE-NUMBER-OF-TARGET-NODES-AFTER-CONNECTING-TREES-II Stats: Time: 272 ms (97.28%), Space: 343.4 MB (48.3%) - LeetHub

--- 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
+++ 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    unordered_map<int, vector<int>> getAdj(vector<vector<int>>& edges) {
-        unordered_map<int, vector<int>> adj;
+    vector<vector<int>> getAdj(vector<vector<int>>& edges, int n) {
+        vector<vector<int>> adj(n);
         for (auto& edge : edges) {
             int u = edge[0];
             int v = edge[1];
-
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
         return adj;
     }
 
-    void dfs(int curr, unordered_map<int, vector<int>>& adj, int parent,
-             vector<int>& mark, int &zeroMarkedCountA, int &oneMarkedCountA) {
+    // O(V+E)
+    void dfs(int curr, vector<vector<int>>& adj, int parent, vector<int>& mark,
+             int& zeroMarkedCountA, int& oneMarkedCountA) {
 
         if (mark[curr] == 0) {
             zeroMarkedCountA++;
@@ -36,26 +36,26 @@ public:
         int N = edges1.size() + 1;
         int M = edges2.size() + 1;
 
-        unordered_map<int, vector<int>> adjA = getAdj(edges1);
-        unordered_map<int, vector<int>> adjB = getAdj(edges2);
+        vector<vector<int>> adjA = getAdj(edges1,N);
+        vector<vector<int>> adjB = getAdj(edges2,M);
 
         vector<int> markA(N, -1);
         markA[0] = 0; // mark 0th node as 0
         int zeroMarkedCountA = 0;
         int oneMarkedCountA = 0;
 
-        dfs(0, adjA, -1, markA, zeroMarkedCountA, oneMarkedCountA);
+        dfs(0, adjA, -1, markA, zeroMarkedCountA, oneMarkedCountA); // O(V+E)
 
         vector<int> markB(M, -1);
         markB[0] = 0; // mark 0th node as 0
         int zeroMarkedCountB = 0;
         int oneMarkedCountB = 0;
 
-        dfs(0, adjB, -1, markB, zeroMarkedCountB, oneMarkedCountB);
+        dfs(0, adjB, -1, markB, zeroMarkedCountB, oneMarkedCountB); // O(V+E)
 
         int maxFromTree2 = max(zeroMarkedCountB, oneMarkedCountB);
         vector<int> result(N);
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) { // O(V)
             int marking = markA[i];
             if (marking == 0) {
                 result[i] = zeroMarkedCountA + maxFromTree2;
