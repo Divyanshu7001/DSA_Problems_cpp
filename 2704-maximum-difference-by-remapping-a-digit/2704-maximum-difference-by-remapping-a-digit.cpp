399484511191a6dc9efa61edfe89eba5a521e79e
Divyanshu Kumar Verma <153366809+Divyanshu7001@users.noreply.github.com>
2025-06-14 18:09:20 +0530

    2704-MAXIMUM-DIFFERENCE-BY-REMAPPING-A-DIGIT Stats: Time: 0 ms (100%), Space: 8.2 MB (37.74%) - LeetHub

--- 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.cpp
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.cpp
@@ -1,26 +1,3 @@
-// Standard Library approach
-// T.c=(log(num))
-// S.C=(log(num))
-// class Solution {
-// public:
-//     int minMaxDifference(int num) {
-
-//         string str1 = to_string(num);
-//         string str2 = str1;
-
-//         int idx = str1.find_first_not_of('9');
-//         if (idx != string::npos) {
-//             char ch = str1[idx];
-//             replace(begin(str1), end(str1), ch, '9');
-//         }
-
-//         char ch = str2[0];
-//         replace(begin(str2), end(str2), ch, '0');
-
-//         return stoi(str1) - stoi(str2);
-//     }
-// };
-
 // Iterative approach
 // T.C=S.C=O(Log(Num))
 class Solution {
@@ -56,3 +33,28 @@ public:
         return stoi(str1) - stoi(str2);
     }
 };
+
+
+// Standard Library approach
+// T.c=(log(num))
+// S.C=(log(num))
+// class Solution {
+// public:
+//     int minMaxDifference(int num) {
+
+//         string str1 = to_string(num);
+//         string str2 = str1;
+
+//         int idx = str1.find_first_not_of('9');
+//         if (idx != string::npos) {
+//             char ch = str1[idx];
+//             replace(begin(str1), end(str1), ch, '9');
+//         }
+
+//         char ch = str2[0];
+//         replace(begin(str2), end(str2), ch, '0');
+
+//         return stoi(str1) - stoi(str2);
+//     }
+// };
+
