b12a8396af269f74d7495fe7ec84462c780f21f1
Divyanshu Kumar Verma <153366809+Divyanshu7001@users.noreply.github.com>
2025-06-14 18:05:32 +0530

    2704-MAXIMUM-DIFFERENCE-BY-REMAPPING-A-DIGIT Stats: Time: 0 ms (100%), Space: 8.2 MB (64.15%) - LeetHub

--- 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.cpp
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.cpp
@@ -1,27 +1,6 @@
 // Standard Library approach
-//T.c=(log(num))
-//S.C=(log(num))
-class Solution {
-public:
-    int minMaxDifference(int num) {
-
-        string str1 = to_string(num);
-        string str2 = str1;
-
-        int idx = str1.find_first_not_of('9');
-        if (idx != string::npos) {
-            char ch = str1[idx];
-            replace(begin(str1), end(str1), ch, '9');
-        }
-
-        char ch = str2[0];
-        replace(begin(str2), end(str2), ch, '0');
-
-        return stoi(str1) - stoi(str2);
-    }
-};
-
-
+// T.c=(log(num))
+// S.C=(log(num))
 // class Solution {
 // public:
 //     int minMaxDifference(int num) {
@@ -41,3 +20,46 @@ public:
 //         return stoi(str1) - stoi(str2);
 //     }
 // };
+
+class Solution {
+public:
+    int minMaxDifference(int num) {
+
+        string str1 = to_string(num);
+        string str2 = str1;
+
+        int n = str1.length();
+
+        // Maximizing String1 by changing most significant bit which is not 9 to
+        // 9
+        for (int i = 0; i < n; i++) {
+            if (str1[i] != '9') {
+                char valueToMaximize = str1[i];
+                for (int j = i; j < n; j++) {
+                    if (str1[j] == valueToMaximize) {
+                        str1[j] = '9';
+                    }
+                }
+                break;
+            }
+        }
+
+        char valueToMinimize = str2[0];
+        for (int j = 0; j < n; j++) {
+            if (str2[j] == valueToMinimize) {
+                str2[j] = '0';
+            }
+        }
+
+        // int idx = str1.find_first_not_of('9');
+        // if (idx != string::npos) {
+        //     char ch = str1[idx];
+        //     replace(begin(str1), end(str1), ch, '9');
+        // }
+
+        // char ch = str2[0];
+        // replace(begin(str2), end(str2), ch, '0');
+
+        return stoi(str1) - stoi(str2);
+    }
+};
