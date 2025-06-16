08630efc6080e1bc67dc8e1f5fa1f653a0f977e7
Divyanshu Kumar Verma <153366809+Divyanshu7001@users.noreply.github.com>
2025-06-16 12:55:01 +0530

    2144-MAXIMUM-DIFFERENCE-BETWEEN-INCREASING-ELEMENTS Stats: Time: 0 ms (100%), Space: 11.9 MB (38.11%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
@@ -1,15 +1,38 @@
+// Brute Force
+
+// class Solution {
+// public:
+//     int maximumDifference(vector<int>& nums) {
+
+//         int maxDifference = -1;
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             for (int j = i + 1; j < nums.size(); j++) {
+//                 if (i < j && (nums[i] < nums[j]) &&
+//                     ((nums[j] - nums[i]) > maxDifference)) {
+//                     maxDifference = nums[j] - nums[i];
+//                 }
+//             }
+//         }
+//         return maxDifference;
+//     }
+// };
+
+
+//Normal solution - O(n)
 class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
 
+        int minElement = nums[0]; // nums[i] taken to be minimum element at
+                                  // first
         int maxDifference = -1;
 
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (i < j && (nums[i] < nums[j]) &&
-                    ((nums[j] - nums[i]) > maxDifference)) {
-                    maxDifference = nums[j] - nums[i];
-                }
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[j] > minElement) {
+                maxDifference = max(maxDifference, nums[j] - minElement);
+            } else {
+                minElement = nums[j];
             }
         }
         return maxDifference;
