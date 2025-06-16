95a0fc25bcbba15fc2fb89baa778cfd5b409b67e
Divyanshu Kumar Verma <153366809+Divyanshu7001@users.noreply.github.com>
2025-06-16 12:40:01 +0530

    2144-MAXIMUM-DIFFERENCE-BETWEEN-INCREASING-ELEMENTS Stats: Time: 7 ms (7.64%), Space: 11.9 MB (71.76%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
@@ -3,20 +3,15 @@ public:
     int maximumDifference(vector<int>& nums) {
 
         int maxDifference = -1;
-        int ithPos = 0, jthPos = 0;
 
         for (int i = 0; i < nums.size(); i++) {
             for (int j = i + 1; j < nums.size(); j++) {
-                if (i < j && (nums[i]<nums[j])&& ((nums[j] - nums[i]) > maxDifference)) {
-                    ithPos = i;
-                    jthPos = j;
+                if (i < j && (nums[i] < nums[j]) &&
+                    ((nums[j] - nums[i]) > maxDifference)) {
                     maxDifference = nums[j] - nums[i];
                 }
             }
         }
-        cout << "Ith: " << ithPos << endl;
-        cout << "Jth: " << jthPos << endl;
-        cout << "Max Diff: " << maxDifference;
         return maxDifference;
     }
 };
