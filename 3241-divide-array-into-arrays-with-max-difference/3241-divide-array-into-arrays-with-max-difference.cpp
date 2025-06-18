2cf402f0af6e5611dc2f193b0a50474ec946404e
Divyanshu Kumar Verma <153366809+Divyanshu7001@users.noreply.github.com>
2025-06-18 22:31:02 +0530

    3241-DIVIDE-ARRAY-INTO-ARRAYS-WITH-MAX-DIFFERENCE Stats: Time: 45 ms (74.64%), Space: 120.1 MB (44.5%) - LeetHub

--- 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.cpp
@@ -2,44 +2,70 @@
 //Hold in the map..loop through
 //Push_back into result when satisfies the condition
 
+// class Solution {
+// public:
+//     vector<vector<int>> divideArray(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         int numberOfArrays = n / 3;
+//         map<int, int> mp;
+//         for (int num : nums) {
+//             mp[num]++;
+//         }
+
+//         vector<vector<int>> result;
+
+//         while (numberOfArrays > 0) {
+//             vector<int> temp;
+//             bool groupFormed = false;
+
+//             for (auto pair = mp.begin(); pair != mp.end();) {
+//                 if (temp.empty() ||
+//                     (pair->first - temp.front() <= k && temp.size() < 3)) {
+//                     temp.push_back(pair->first);
+//                     pair->second--;
+//                     if (pair->second == 0) {
+//                         pair = mp.erase(pair);
+//                     } 
+//                     if (temp.size() == 3) {
+//                         result.push_back(temp);
+//                         numberOfArrays--;
+//                         groupFormed = true;
+//                         break;
+//                     }
+//                 } else {
+//                     ++pair;
+//                 }
+//             }
+
+//             if (!groupFormed) {
+//                 return {};
+//             }
+//         }
+
+//         return result;
+//     }
+// };
+
+//T.C=
+//The difference between any two elements in one array is less than or equal to k.
+//Used the above line as the main point of reference
 class Solution {
 public:
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
-        int n = nums.size();
-        int numberOfArrays = n / 3;
-        map<int, int> mp;
-        for (int num : nums) {
-            mp[num]++;
-        }
-
+        sort(nums.begin(), nums.end());
         vector<vector<int>> result;
 
-        while (numberOfArrays > 0) {
-            vector<int> temp;
-            bool groupFormed = false;
+        for (int i = 0; i < nums.size(); i += 3) {
+            int a = nums[i];
+            int b = nums[i + 1];
+            int c = nums[i + 2];
 
-            for (auto pair = mp.begin(); pair != mp.end();) {
-                if (temp.empty() ||
-                    (pair->first - temp.front() <= k && temp.size() < 3)) {
-                    temp.push_back(pair->first);
-                    pair->second--;
-                    if (pair->second == 0) {
-                        pair = mp.erase(pair);
-                    } 
-                    if (temp.size() == 3) {
-                        result.push_back(temp);
-                        numberOfArrays--;
-                        groupFormed = true;
-                        break;
-                    }
-                } else {
-                    ++pair;
-                }
+            // Check max difference in group
+            if (c - a > k) {
+                return {}; // invalid group
             }
 
-            if (!groupFormed) {
-                return {};
-            }
+            result.push_back({a, b, c});
         }
 
         return result;
