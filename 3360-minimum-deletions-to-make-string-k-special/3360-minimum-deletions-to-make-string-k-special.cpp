88f311b6b691036cd249d401512e206147d98307
Divyanshu Kumar Verma <153366809+Divyanshu7001@users.noreply.github.com>
2025-06-21 20:15:00 +0530

    3360-MINIMUM-DELETIONS-TO-MAKE-STRING-K-SPECIAL Stats: Time: 8 ms (56.88%), Space: 19.3 MB (70.29%) - LeetHub

--- 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
@@ -12,41 +12,80 @@
 
 // Return the minimum value
 
+// class Solution {
+// public:
+//     int minimumDeletions(string word, int k) {
+
+//         unordered_map<char,int> freq;
+//         for(char c:word){
+//             frequencies[c-'a']++;
+//         }
+
+//         vector<int>frequencies;
+//         for(auto& pair:freq){
+//             frequencies.push_back(pair.second);
+//         }
+
+//         sort(frequencies.begin(),frequencies.end());
+
+//         int minDeletions=INT_MAX;
+//         int n=frequencies.size();
+
+//         for(int i=0;i<n;i++){
+//             int base=frequencies[i];
+//             int totalDeletions=0;
+
+//             for(int j=0;j<i;j++){
+//                 totalDeletions+=frequencies[j];
+//             }
+
+//             for(int j=i;j<n;j++){
+//                 if(frequencies[j]>base+k){
+//                     totalDeletions+=frequencies[j]-(base+k);
+//                 }
+//             }
+
+//             if(totalDeletions<minDeletions){
+//                 minDeletions=totalDeletions;
+//             }
+//         }
+
+//         return minDeletions;
+//     }
+// };
+
 class Solution {
 public:
     int minimumDeletions(string word, int k) {
 
-        unordered_map<char,int> freq;
-        for(char c:word){
-            freq[c]++;
+        vector<int> frequencies(26, 0);
+        for (char c : word) {
+            frequencies[c - 'a']++;
         }
+        
+        sort(frequencies.begin(), frequencies.end());
 
-        vector<int>frequencies;
-        for(auto& pair:freq){
-            frequencies.push_back(pair.second);
-        }
+        int minDeletions = INT_MAX;
+        int n = frequencies.size();
 
-        sort(frequencies.begin(),frequencies.end());
+        for (int i = 0; i < n; i++) {
+            int base = frequencies[i];
+            // if (base == 0)
+            //     break;
+            int totalDeletions = 0;
 
-        int minDeletions=INT_MAX;
-        int n=frequencies.size();
-
-        for(int i=0;i<n;i++){
-            int base=frequencies[i];
-            int totalDeletions=0;
-
-            for(int j=0;j<i;j++){
-                totalDeletions+=frequencies[j];
+            for (int j = 0; j < i; j++) {
+                totalDeletions += frequencies[j];
             }
 
-            for(int j=i;j<n;j++){
-                if(frequencies[j]>base+k){
-                    totalDeletions+=frequencies[j]-(base+k);
+            for (int j = i; j < n; j++) {
+                if (frequencies[j] > base + k) {
+                    totalDeletions += frequencies[j] - (base + k);
                 }
             }
 
-            if(totalDeletions<minDeletions){
-                minDeletions=totalDeletions;
+            if (totalDeletions < minDeletions) {
+                minDeletions = totalDeletions;
             }
         }
 
