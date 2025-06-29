11537fa573d75835c2c5ed1d79bae2b0b0622464
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-06-29 14:16:54 +0530

    1621-NUMBER-OF-SUBSEQUENCES-THAT-SATISFY-THE-GIVEN-SUM-CONDITION Stats: Time: 31 ms (62.49%), Space: 54.4 MB (34.63%) - LeetHub

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,3 +1,18 @@
+// Bruete Forc/ Simulation is too big
+// Binary search approach with number of sequences pre-determined in a vector
+
+// Approach:
+// 1.Sort the arrray
+// hold the power according to the index element i.e in 2^i power
+// 1->2^1=2
+// 2->2^2=4
+// 3->2^3=8 etc..
+
+// have a integer value rersult to hold number of subsequences
+// keep doing binary search
+// as vector is sorted i.e min element at first and maximum at last
+// if vector[left]=vector[right]<=target ...left forward & add sequences number
+// into result else right backward
 class Solution {
 public:
     int numSubseq(vector<int>& nums, int target) {
