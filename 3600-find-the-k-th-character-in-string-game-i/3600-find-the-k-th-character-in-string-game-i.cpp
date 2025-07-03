b14e32f78b3e31970dc07304d58b9d07b84b6a2b
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-07-03 17:31:48 +0530

    3600-FIND-THE-K-TH-CHARACTER-IN-STRING-GAME-I Stats: Time: 6 ms (21.87%), Space: 10.8 MB (19.99%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,51 +1,47 @@
-// class Solution {
-// public:
-//     char last = 'a';
-//     string word = "a";
-
-//     void recurse(string& word, int k) {
-//         cout << word;
-//         if (word.length() == k) {
-//             return;
-//         }
-//         for (int i = 0; i < word.length(); i++) {
-//             char ch = word[i];
-//             cout << "Character: " << ch;
-//             ch == 'z' ? last = 'a' : last = ch + 1;
-//             word += last;
-//             if (word.length() == k)
-//                 return;
-//         }
-
-//         recurse(word, k);
-//     }
-
-//     char kthCharacter(int k) {
-//         if (word.length() == k) {
-//             return 'a';
-//         }
-//         // char out = 'a' + 1;
-//         // cout << out;
-//         recurse(word, k);
-//         return last;
-//     }
-// };
-
 class Solution {
 public:
-    char nextChar(char c) {
-        return (c == 'z') ? 'a' : c + 1;
+    string word = "a";
+
+    void recurse(string& word, int k) {
+        if (word.length() >= k) {
+            return;
+        }
+
+        string temp;
+        for (char ch : word) {
+            char next = (ch == 'z') ? 'a' : ch + 1;
+            temp += next;
+        }
+        word += temp;
+        recurse(word, k);
     }
 
     char kthCharacter(int k) {
-        string word = "a";
-        while (word.length() < k) {
-            string temp;
-            for (char c : word) {
-                temp += nextChar(c);
-            }
-            word += temp;
+        if (word.length() >= k) {
+            return word[k - 1];
         }
-        return word[k - 1];
+
+        recurse(word, k);
+        return word[k-1];
     }
 };
+
+//T.C=O(k)
+// class Solution {
+// public:
+//     char nextChar(char c) {
+//         return (c == 'z') ? 'a' : c + 1;
+//     }
+
+//     char kthCharacter(int k) {
+//         string word = "a";
+//         while (word.length() < k) {
+//             string temp;
+//             for (char c : word) {
+//                 temp += nextChar(c);
+//             }
+//             word += temp;
+//         }
+//         return word[k - 1];
+//     }
+// };
