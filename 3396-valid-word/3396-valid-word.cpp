077112625aba91c2b1084de3f7b8fbc41cb7b330
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-07-15 19:05:34 +0530

    3396-VALID-WORD Stats: Time: 1 ms (16.26%), Space: 7.9 MB (87.14%) - LeetHub

--- 3396-valid-word/3396-valid-word.cpp
+++ 3396-valid-word/3396-valid-word.cpp
@@ -19,6 +19,6 @@ public:
                     hasConsonent = true;
             }
         }
-        return (hasVowel && hasConsonent) ? true : false;
+        return hasVowel && hasConsonent;
     }
 };
