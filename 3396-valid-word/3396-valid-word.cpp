e9b60394b8f277f6c0fb0fc6dc3d1bb9dcec9067
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-07-15 18:50:50 +0530

    3396-VALID-WORD Stats: Time: 0 ms (100%), Space: 8 MB (67.48%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string word) {
+        bool hasVowel = false, hasConsonent = false;
+
+        if (word.length() < 3) {
+            return false;
+        }
+
+        for (char ch : word) {
+            if (!isalnum(ch)) {
+                return false;
+            } else if (isalpha(ch)) {
+                ch = tolower(ch);
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' ||
+                    ch == 'u')
+                    hasVowel = true;
+                else
+                    hasConsonent = true;
+            }
+        }
+        return (hasVowel && hasConsonent) ? true : false;
+    }
+};
