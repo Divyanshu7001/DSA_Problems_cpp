9d6cbb182782e643a863955197dd19b0e48baaa8
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-07-21 13:33:24 +0530

    1302-DELETE-CHARACTERS-TO-MAKE-FANCY-STRING Stats: Time: 20 ms (84.63%), Space: 44.6 MB (13.57%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -22,44 +22,25 @@
 // };
 
 //New result string making
-// class Solution {
-// public:
-//     string makeFancyString(string s) {
-//         int freqCount = 1;
-//         int i = 1;
-//         string result;
-//         result.push_back(s[0]);
-//         while (i < s.size()) {
-//             if (s[i] == s[i - 1]) {
-//                 ++freqCount;
-//                 if (freqCount > 2) {
-//                     i++;
-//                 } else
-//                     result.push_back(s[i]);
-//             } else {
-//                 result.push_back(s[i]);
-//                 freqCount = 1;
-//                 i++;
-//             }
-//         }
-//         return result;
-//     }
-// };
-
-
-//Optimal new string making
 class Solution {
 public:
-    string makeFancyString(const string &s) {
+    string makeFancyString(string s) {
+        int freqCount = 1;
+        int i = 1;
         string result;
-        result.reserve(s.size());
-
-        for (char c : s) {
-            int len = result.size();
-            if (len >= 2 && result[len - 1] == c && result[len - 2] == c) {
-                continue;
+        result.push_back(s[0]);
+        while (i < s.size()) {
+            if (s[i] == s[i - 1]) {
+                ++freqCount;
+                if (freqCount > 2) {
+                    i++;
+                } else
+                    result.push_back(s[i]);
+            } else {
+                result.push_back(s[i]);
+                freqCount = 1;
+                i++;
             }
-            result.push_back(c);
         }
         return result;
     }
