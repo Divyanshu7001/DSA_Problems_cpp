d9a398aaae27fe6d156fd667599d00569de57fc4
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-07-21 13:34:43 +0530

    1302-DELETE-CHARACTERS-TO-MAKE-FANCY-STRING Stats: Time: 44 ms (16.97%), Space: 44.6 MB (39.72%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,9 +1,9 @@
-//In-string update
-// class Solution {
-// public:
-//     string makeFancyString(string s) {
-//         int n = s.length();
-//         int freqCount = 1;
+// In-string update
+//  class Solution {
+//  public:
+//      string makeFancyString(string s) {
+//          int n = s.length();
+//          int freqCount = 1;
 
 //         for (int i = 1; i <= n; i++) {
 //             if (s[i] == s[i - 1]) {
@@ -21,23 +21,23 @@
 //     }
 // };
 
-//New result string making
+// New result string making
 class Solution {
 public:
     string makeFancyString(string s) {
         int freqCount = 1;
         int i = 1;
-        string result;
-        result.push_back(s[0]);
+        string result = "";
+        result += s[0];
         while (i < s.size()) {
             if (s[i] == s[i - 1]) {
                 ++freqCount;
                 if (freqCount > 2) {
                     i++;
                 } else
-                    result.push_back(s[i]);
+                    result += s[i];
             } else {
-                result.push_back(s[i]);
+                result += s[i];
                 freqCount = 1;
                 i++;
             }
