adeadb171fb6724ac2ff87c12ce1a75d88779b1c
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-07-26 18:23:09 +0530

    3789-MAXIMIZE-SUBARRAYS-AFTER-REMOVING-ONE-CONFLICTING-PAIR Stats: Time: 160 ms (67.8%), Space: 324.4 MB (28.81%) - LeetHub

--- 3789-maximize-subarrays-after-removing-one-conflicting-pair/3789-maximize-subarrays-after-removing-one-conflicting-pair.cpp
+++ 3789-maximize-subarrays-after-removing-one-conflicting-pair/3789-maximize-subarrays-after-removing-one-conflicting-pair.cpp
@@ -8,6 +8,10 @@
 // some conflicting point
 
 // At the last valid += max of the extra subarray global list
+
+// T.C = O(n)+O(n+p)+O(n) = o(n)
+
+#define SORT(x) begin(x),end(x)
 class Solution {
 public:
     long long maxSubarrays(int n, vector<vector<int>>& conflictingPairs) {
@@ -16,6 +20,7 @@ public:
         vector<vector<int>> conflictingPoints(n + 1);
         // conflictingPoints[i]=points which coflict with i
 
+        // O(n)
         for (auto& p : conflictingPairs) {
             //(a,b)
             int a = min(p[0], p[1]);
@@ -33,12 +38,15 @@ public:
         // extra[i]=how many extra subarrays we can get by removing the
         // perticular i conflict point
 
+        // overall = O(n+p)
+        // O(n)
         for (int end = 1; end <= n;
              end++) { // Check all the conflicting points of end(as a point) by
                       // going into the list of conflictingPoints for this
                       // perticular end point
 
-            // Total subarrays ending at the point end
+            // O(p)
+            //  Total subarrays ending at the point end
             for (int& u : conflictingPoints[end]) {
                 // in the list of conflicting points of end
                 if (u >= maxConflict) {
@@ -53,6 +61,7 @@ public:
             extra[maxConflict] += maxConflict - secondMaxConflict;
         }
 
-        return valid + *max_element(begin(extra), end(extra));
+        // for max_element=O(n)
+        return valid + *max_element(SORT(extra));
     }
 };
