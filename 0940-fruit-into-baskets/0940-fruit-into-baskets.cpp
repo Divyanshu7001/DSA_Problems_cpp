f50246a73eaef6296985d0d516029bc451c121a1
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-08-04 19:03:15 +0530

    0940-FRUIT-INTO-BASKETS Stats: Time: 39 ms (63.3%), Space: 75.1 MB (31.99%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -3,54 +3,55 @@
 
 // T.C=O(n)
 // S.C=O(1)
-// class Solution {
-// public:
-//     int totalFruit(vector<int>& fruits) {
-//         if (fruits.size() == 1) {
-//             return 1;
-//         }
-//         unordered_map<int, int> basket;
-//         int maxLen = 0, left = 0;
-//         basket[fruits[left]]++;
-//         for (int right = left + 1; right < fruits.size(); right++) {
-//             basket[fruits[right]]++;
-//             while (basket.size() > 2) {
-//                 basket[fruits[left]]--;
-//                 if (basket[fruits[left]] == 0) {
-//                     basket.erase(fruits[left]);
-//                 }
-//                 left++;
-//             }
-//             maxLen = max(maxLen, right - left + 1);
-//         }
-//         return maxLen;
-//     }
-// };
-
 class Solution {
 public:
-    int totalFruit(vector<int>& arr) {
-        int lastFruit = -1, secondLastFruit = -1;
-        int lastFruitCount = 0;
-        int maxCount = 0, curCount = 0;
-
-        for (int fruit : arr) {
-            if (fruit == lastFruit || fruit == secondLastFruit) {
-                curCount++;
-            } else {
-                curCount = lastFruitCount + 1; // just last streak + new fruit
-            }
-
-            if (fruit == lastFruit)
-                lastFruitCount++;
-            else {
-                lastFruitCount = 1;
-                secondLastFruit = lastFruit;
-                lastFruit = fruit;
-            }
-
-            maxCount = max(maxCount, curCount);
+    int totalFruit(vector<int>& fruits) {
+        if (fruits.size() == 1) {
+            return 1;
         }
-        return maxCount;
+        unordered_map<int, int> basket;
+        int maxLen = 0, left = 0;
+        basket[fruits[left]]++;
+        for (int right = left + 1; right < fruits.size(); right++) {
+            basket[fruits[right]]++;
+            while (basket.size() > 2) {
+                basket[fruits[left]]--;
+                if (basket[fruits[left]] == 0) {
+                    basket.erase(fruits[left]);
+                }
+                left++;
+            }
+            maxLen = max(maxLen, right - left + 1);
+        }
+        return maxLen;
     }
 };
+
+//A bit complicated but tracking last and secondLastFruit ..accordingly increase or reset currCount
+// class Solution {
+// public:
+//     int totalFruit(vector<int>& arr) {
+//         int lastFruit = -1, secondLastFruit = -1;
+//         int lastFruitCount = 0;
+//         int maxCount = 0, curCount = 0;
+
+//         for (int fruit : arr) {
+//             if (fruit == lastFruit || fruit == secondLastFruit) {
+//                 curCount++;
+//             } else {
+//                 curCount = lastFruitCount + 1; // Last streak + new fruit
+//             }
+
+//             if (fruit == lastFruit)
+//                 lastFruitCount++;
+//             else {
+//                 lastFruitCount = 1;
+//                 secondLastFruit = lastFruit;
+//                 lastFruit = fruit;
+//             }
+
+//             maxCount = max(maxCount, curCount);
+//         }
+//         return maxCount;
+//     }
+// };
