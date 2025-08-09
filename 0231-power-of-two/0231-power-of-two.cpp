46de32c721d2ca7bebd97f6181dcd7a8cd75c101
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-08-09 13:41:55 +0530

    0231-POWER-OF-TWO Stats: Time: 0 ms (100%), Space: 8 MB (17.71%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,18 +1,33 @@
+//T.c=O(logn)
+// class Solution {
+// public:
+//     bool checkPower(int n, int a) {
+//         auto result = pow(2, a);
+//         if (result == n) {
+//             return true;
+//         } else if (result > n) {
+//             return false;
+//         } else
+//             return checkPower(n, a + 1);
+//     }
+//     bool isPowerOfTwo(int n) {
+//         if (n == 1 || n == 2)
+//             return true;
+//         bool result = checkPower(n, 2);
+//         return result;
+//     }
+// };
+
+
+// In binary representation, powers of two have a distinctive property:
+
+// If n is a power of two, it contain exactly one bit set to 1.
+// then, if we perform AND operation of n with nâ1, it will flip the set bit. ââ
+// This unique pattern allows us to detect powers of two efficiently using bitwise operations.
+//T.C=O(1)
 class Solution {
 public:
-    bool checkPower(int n, int a) {
-        auto result = pow(2, a);
-        if (result == n) {
-            return true;
-        } else if (result > n) {
-            return false;
-        } else
-            return checkPower(n, a + 1);
-    }
     bool isPowerOfTwo(int n) {
-        if (n == 1 || n == 2)
-            return true;
-        bool result = checkPower(n, 2);
-        return result;
+        return n > 0 && (n & (n - 1)) == 0;
     }
 };
