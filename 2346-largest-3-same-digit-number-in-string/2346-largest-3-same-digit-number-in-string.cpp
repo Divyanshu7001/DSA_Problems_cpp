11d0d154d733e6eee25ab62045ebc1729d57278a
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-08-14 17:36:08 +0530

    2346-LARGEST-3-SAME-DIGIT-NUMBER-IN-STRING Stats: Time: 0 ms (100%), Space: 8.9 MB (96.52%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
@@ -1,31 +1,49 @@
+//Two pointers maintainig method
+// class Solution {
+// public:
+//     string largestGoodInteger(string num) {
+//         string maxElement = "";
+//         int n = num.length();
+//         int i = 0, j = 0, count = 0;
+//         while (i < n && j < n) {
+//             string temp = "";
+//             if (num[i] == num[j]) {
+//                 count++;
+//                 if (count == 3) {
+//                     if (num.substr(i, 3) > temp) {
+//                         temp = num.substr(i, 3);
+//                         maxElement = max(maxElement, temp);
+//                     }
+//                     i = j;
+//                     j++;
+//                     temp.clear();
+//                     count = 0;
+//                 } else {
+//                     j++;
+//                 }
+//             } else {
+//                 i++;
+//                 count = 0;
+//             }
+//         }
+//         return maxElement;
+//     }
+// };
+
+//one pointer index maintain ..according just to this problem with 3 indexes
 class Solution {
 public:
     string largestGoodInteger(string num) {
         string maxElement = "";
         int n = num.length();
-        int i = 0, j = 0, count = 0;
-        while (i < n && j < n) {
-            string temp = "";
-            if (num[i] == num[j]) {
-                count++;
-                cout << "Count value: " << count;
-                if (count == 3) {
-                    if (num.substr(i, 3) > temp) {
-                        temp = num.substr(i, 3);
-                        maxElement = max(maxElement, temp);
-                    }
-                    i = j;
-                    j++;
-                    temp.clear();
-                    count = 0;
-                } else {
-                    j++;
-                }
-            } else {
-                i++;
-                count = 0;
+
+        for (int i = 0; i <= n - 3; i++) {
+            if (num[i] == num[i+1] && num[i] == num[i+2]) {
+                string temp = num.substr(i, 3); // exactly 3 characters
+                maxElement = max(maxElement, temp); // lexicographic works for equal-length strings
             }
         }
+
         return maxElement;
     }
 };
