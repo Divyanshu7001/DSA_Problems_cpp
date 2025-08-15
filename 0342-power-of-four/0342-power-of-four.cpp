13e8a4f6ef8838b79589bb696d52f2c8d921c634
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-08-15 13:25:51 +0530

    0342-POWER-OF-FOUR Stats: Time: 28 ms (0.2%), Space: 8 MB (18.14%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,3 +1,4 @@
+//T.C=O(n)
 class Solution {
 public:
     const long long LIMIT = (1LL << 31) - 1;
@@ -5,6 +6,7 @@ public:
         if (n == 1)
             return true;
         for (long long i = 4; i < LIMIT; i <<= 2) {
+            cout<<i<<endl;
             if (i == n) {
                 return true;
                 break;
