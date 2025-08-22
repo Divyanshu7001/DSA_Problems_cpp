482a71aa329501bad136a244b8d1e411412f11e3
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-08-22 23:13:24 +0530

    3461-FIND-THE-MINIMUM-AREA-TO-COVER-ALL-ONES-I Stats: Time: 244 ms (89.46%), Space: 133.6 MB (12.14%) - LeetHub

--- 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -1,30 +1,51 @@
+//A bit more expensive on the traversal side
+//T.C = O(nÂ²) or O(mn)
+// class Solution {
+// public:
+//     int minimumArea(vector<vector<int>>& grid) {
+//         int m = grid.size();
+//         int n = grid[0].size();
+
+//         int minRow = m, maxRow = -1;
+//         int minCol = n, maxCol = -1;
+
+//         // Traverse the grid to locate all 1s
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if (grid[i][j] == 1) {
+//                     minRow = min(minRow, i);
+//                     maxRow = max(maxRow, i);
+//                     minCol = min(minCol, j);
+//                     maxCol = max(maxCol, j);
+//                 }
+//             }
+//         }
+
+//         // If no 1 found, area is 0 (depends on problem statement)
+//         if (maxRow == -1)
+//             return 0;
+
+//         int height = maxRow - minRow + 1;
+//         int width = maxCol - minCol + 1;
+//         return height * width;
+//     }
+// };
+
+
 class Solution {
 public:
     int minimumArea(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-
-        int minRow = m, maxRow = -1;
-        int minCol = n, maxCol = -1;
-
-        // Traverse the grid to locate all 1s
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    minRow = min(minRow, i);
-                    maxRow = max(maxRow, i);
-                    minCol = min(minCol, j);
-                    maxCol = max(maxCol, j);
+        int minrow=INT_MAX,maxrow=INT_MIN,mincol=INT_MAX,maxcol=INT_MIN;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==1){
+                    minrow=min(i,minrow);
+                    maxrow=max(i,maxrow);
+                    mincol=min(j,mincol);
+                    maxcol=max(j,maxcol);
                 }
             }
         }
-
-        // If no 1 found, area is 0 (depends on problem statement)
-        if (maxRow == -1)
-            return 0;
-
-        int height = maxRow - minRow + 1;
-        int width = maxCol - minCol + 1;
-        return height * width;
+        return (maxrow-minrow+1)*(maxcol-mincol+1);
     }
 };
