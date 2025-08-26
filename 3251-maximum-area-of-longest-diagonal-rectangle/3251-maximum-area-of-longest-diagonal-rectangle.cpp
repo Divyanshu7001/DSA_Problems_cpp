10db33f7f82f199bb89515a19d4859e022e9dab9
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-08-26 21:53:28 +0530

    3251-MAXIMUM-AREA-OF-LONGEST-DIAGONAL-RECTANGLE Stats: Time: 0 ms (100%), Space: 29.4 MB (52.12%) - LeetHub

--- 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.cpp
+++ 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.cpp
@@ -4,12 +4,12 @@ public:
         int maxDiagonal = -1, maxArea = 0;
 
         for (auto& dimension : dimensions) {
-            int diag =
+            int currentDiagonal =
                 dimension[0] * dimension[0] + dimension[1] * dimension[1];
             int area = dimension[0] * dimension[1];
 
-            if (diag > maxDiagonal || (diag == maxDiagonal && area > maxArea)) {
-                maxDiagonal = diag;
+            if (currentDiagonal > maxDiagonal || (currentDiagonal == maxDiagonal && area > maxArea)) {
+                maxDiagonal = currentDiagonal;
                 maxArea = area;
             }
         }
