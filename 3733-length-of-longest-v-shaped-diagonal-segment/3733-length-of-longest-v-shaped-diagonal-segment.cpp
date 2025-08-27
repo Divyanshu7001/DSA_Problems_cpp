5cce2fa1cc6d9183fb48563e2fe3bd126e8c6139
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-08-28 02:28:47 +0530

    3733-LENGTH-OF-LONGEST-V-SHAPED-DIAGONAL-SEGMENT Stats: Time: 263 ms (81.5%), Space: 104.1 MB (66%) - LeetHub

--- 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.cpp
@@ -1,8 +1,13 @@
 // T.C = O(NâM)
+//$ values are of changing parameters i.e row no, column no, direction & can
+// turn or not
 class Solution {
 public:
     vector<vector<int>> directions = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
-    int m, n;
+    int m, n, t[501][501][4][2];
+    // 501,501 as row and column constraints , 4 for 4 kinds of direction
+    // changes possible & 2 for the canTurn values true or false
+
     int solve(int i, int j, int d, bool canTurn, int val,
               vector<vector<int>>& grid) {
         int i_ = i + directions[d][0], j_ = j + directions[d][1];
@@ -10,6 +15,10 @@ public:
         if (i_ < 0 || i_ >= n || j_ < 0 || j_ >= m || grid[i_][j_] != val) {
             return 0;
         }
+
+        if (t[i_][j_][d][canTurn] != -1) {
+            return t[i_][j_][d][canTurn];
+        };
 
         int result = 0,
             keepMoving = 1 + solve(i_, j_, d, canTurn, val == 2 ? 0 : 2, grid);
@@ -21,13 +30,13 @@ public:
             result = max(result, turnAndMove);
         }
 
-        return result;
+        return t[i_][j_][d][canTurn] = result;
     }
 
     int lenOfVDiagonal(vector<vector<int>>& grid) {
         n = grid.size();
         m = grid[0].size();
-
+        memset(t, -1, sizeof(t));
         int res = 0;
         // start from every cell == 1
         for (int i = 0; i < n; i++) {
