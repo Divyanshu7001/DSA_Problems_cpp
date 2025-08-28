fa9aa93184f06cec9a28f7b6302b1039d9173e70
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-08-28 18:35:36 +0530

    3748-SORT-MATRIX-BY-DIAGONALS Stats: Time: 9 ms (60.49%), Space: 45.3 MB (28.19%) - LeetHub

--- 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.cpp
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.cpp
@@ -1,20 +1,58 @@
+// With Vector- very nice solution and easy
+// T.C=O(NlogN) , S.C = O(N)
+//  class Solution {
+//  public:
+//      int n;
+
+//     void solve(int row, int column, vector<vector<int>>& grid,
+//                bool isIncreasingSorting) {
+
+//         vector<int> tempList;
+//         int i = row, j = column;
+//         while (i < n && j < n) {
+//             tempList.push_back(grid[i][j]);
+//             i++;
+//             j++;
+//         }
+//         isIncreasingSorting == true
+//             ? sort(tempList.begin(), tempList.end())
+//             : sort(tempList.begin(), tempList.end(), greater<int>());
+//         i = row, j = column;
+//         for (int val : tempList) {
+//             grid[i][j] = val;
+//             i++;
+//             j++;
+//         }
+//     }
+
+//     vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+//         n = grid.size();
+//         for (int j = 1; j < n - 1; j++) {
+//             solve(0, j, grid, true);
+//         }
+//         for (int i = 0; i < n - 1; i++) {
+//             solve(i, 0, grid, false);
+//         }
+
+//         return grid;
+//     }
+// };
+
 class Solution {
 public:
     int n;
 
-    void solve(int row, int column, vector<vector<int>>& grid,
-               bool isIncreasingSorting) {
+    template <typename Comp>
+    void solve(int row, int column, vector<vector<int>>& grid, Comp comp) {
+        multiset<int, Comp> tempList(comp);
 
-        vector<int> tempList;
         int i = row, j = column;
         while (i < n && j < n) {
-            tempList.push_back(grid[i][j]);
+            tempList.insert(grid[i][j]);
             i++;
             j++;
         }
-        isIncreasingSorting == true
-            ? sort(tempList.begin(), tempList.end())
-            : sort(tempList.begin(), tempList.end(), greater<int>());
+
         i = row, j = column;
         for (int val : tempList) {
             grid[i][j] = val;
@@ -26,12 +64,11 @@ public:
     vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
         n = grid.size();
         for (int j = 1; j < n - 1; j++) {
-            solve(0, j, grid, true);
+            solve(0, j, grid, less<int>());     // ascending
         }
         for (int i = 0; i < n - 1; i++) {
-            solve(i, 0, grid, false);
+            solve(i, 0, grid, greater<int>());  // descending
         }
-
         return grid;
     }
 };
