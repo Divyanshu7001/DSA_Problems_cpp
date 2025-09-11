1f6e4199c5a2af6974aeb46f715e2fb4e0336c2b
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-09-11 17:42:36 +0530

    2887-SORT-VOWELS-IN-A-STRING Stats: Time: 48 ms (5.22%), Space: 24 MB (5.44%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -1,25 +1,52 @@
+// T.C = O(Nlogn)
+//  class Solution {
+//  public:
+//      string sortVowels(string s) {
+//          vector<int> asciiValues;
+//          vector<int> asciiIndexes;
+//          for (int i = 0; i < s.length(); i++) {
+//              if (s[i] == 'a' || s[i] == 'A' || s[i] == 'e' || s[i] == 'E' ||
+//                  s[i] == 'i' || s[i] == 'I' || s[i] == 'o' || s[i] == 'O' ||
+//                  s[i] == 'u' || s[i] == 'U') {
+//                  asciiValues.push_back((int)s[i]);
+//                  asciiIndexes.push_back(i);
+//              }
+//          }
+
+//         if (asciiValues.size() == 0)
+//             return s;
+
+//         sort(asciiValues.begin(), asciiValues.end(), greater<int>());
+
+//         for (int index : asciiIndexes) {
+//             s[index] = asciiValues.back();
+//             asciiValues.pop_back();
+//         }
+//         return s;
+//     }
+// };
+
+// T.C = O(Nlogn)
 class Solution {
 public:
     string sortVowels(string s) {
-        vector<int> asciiValues;
+        priority_queue<int, vector<int>, greater<int>> minHeap;
         vector<int> asciiIndexes;
         for (int i = 0; i < s.length(); i++) {
             if (s[i] == 'a' || s[i] == 'A' || s[i] == 'e' || s[i] == 'E' ||
                 s[i] == 'i' || s[i] == 'I' || s[i] == 'o' || s[i] == 'O' ||
                 s[i] == 'u' || s[i] == 'U') {
-                asciiValues.push_back((int)s[i]);
+                minHeap.push((int)s[i]);
                 asciiIndexes.push_back(i);
             }
         }
 
-        if (asciiValues.size() == 0)
+        if (minHeap.size() == 0)
             return s;
 
-        sort(asciiValues.begin(), asciiValues.end(), greater<int>());
-
         for (int index : asciiIndexes) {
-            s[index] = asciiValues.back();
-            asciiValues.pop_back();
+            s[index] = minHeap.top();
+            minHeap.pop();
         }
         return s;
     }
