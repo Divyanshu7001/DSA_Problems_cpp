9e0ecaa1358dbfc93cc4ad6188c1d3e10b07e2dd
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-09-28 13:49:39 +0530

    1018-LARGEST-PERIMETER-TRIANGLE Stats: Time: 0 ms (100%), Space: 25.5 MB (76.13%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cpp
@@ -16,17 +16,18 @@
 //     }
 // };
 
+// T.C=O(NLogN)
 class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
-        int n = nums.size(), maxPerimeter = 0;
+        int n = nums.size();
         sort(nums.begin(), nums.end());
         for (int i = n - 1; i >= 2; i--) {
             int a = nums[i], b = nums[i - 1], c = nums[i - 2];
             if (a + b > c && b + c > a && c + a > b) {
-                maxPerimeter = max(maxPerimeter, a + b + c);
+                return a + b + c;
             }
         }
-        return maxPerimeter;
+        return 0;
     }
 };
