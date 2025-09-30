5c52b124de661457da9c8c8c502d456e75a07fc1
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-09-30 18:11:54 +0530

    2324-FIND-TRIANGULAR-SUM-OF-AN-ARRAY Stats: Time: 159 ms (28.67%), Space: 144.4 MB (32.44%) - LeetHub

--- 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.cpp
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.cpp
@@ -3,9 +3,9 @@ public:
     int solve(vector<int>& nums) {
         if (nums.size() == 1)
             return nums[0];
-        vector<int> newNums;
+        vector<int> newNums(nums.size() - 1, -1);
         for (int i = 0; i < nums.size() - 1; i++) {
-            newNums.push_back((nums[i] + nums[i + 1]) % 10);
+            newNums[i] = (nums[i] + nums[i + 1]) % 10;
         }
         return solve(newNums);
     }
