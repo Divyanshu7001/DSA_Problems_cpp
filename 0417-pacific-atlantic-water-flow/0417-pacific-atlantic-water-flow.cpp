865a6b3c56bee11959c5a3f1a6649dff39be781d
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-10-06 00:45:14 +0530

    0417-PACIFIC-ATLANTIC-WATER-FLOW Stats: Time: 7 ms (62.44%), Space: 22.2 MB (89.71%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
+    int m, n;
+    vector<pair<int, int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    void dfs(int r, int c, vector<vector<bool>>& visited,
+             vector<vector<int>>& heights) {
+        visited[r][c] = true;
+
+        for (auto [dr, dc] : dirs) {
+            int nr = r + dr, nc = c + dc;
+            if (nr < 0 || nc < 0 || nr >= m || nc >= n)
+                continue;
+            if (visited[nr][nc])
+                continue;
+            if (heights[nr][nc] < heights[r][c])
+                continue; // must flow uphill
+            dfs(nr, nc, visited, heights);
+        }
+    }
+
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int m = heights.size(), n = heights[0].size();
+        m = heights.size();
+        n = heights[0].size();
+
         vector<vector<bool>> pacific(m, vector<bool>(n, false));
         vector<vector<bool>> atlantic(m, vector<bool>(n, false));
 
-        // Directions: up, down, left, right
-        vector<pair<int,int>> dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-
-        auto dfs = [&](auto&& dfs, int r, int c, vector<vector<bool>>& visited) -> void {
-            visited[r][c] = true;
-            for (auto [dr, dc] : dirs) {
-                int nr = r + dr, nc = c + dc;
-                if (nr < 0 || nc < 0 || nr >= m || nc >= n) continue;
-                if (visited[nr][nc]) continue;
-                if (heights[nr][nc] < heights[r][c]) continue; // must flow uphill
-                dfs(dfs, nr, nc, visited);
-            }
-        };
-
         // Start from Pacific (top + left)
-        for (int i = 0; i < m; ++i) dfs(dfs, i, 0, pacific);
-        for (int j = 0; j < n; ++j) dfs(dfs, 0, j, pacific);
+        for (int i = 0; i < m; ++i)
+            dfs(i, 0, pacific, heights);
+        for (int j = 0; j < n; ++j)
+            dfs(0, j, pacific, heights);
 
         // Start from Atlantic (bottom + right)
-        for (int i = 0; i < m; ++i) dfs(dfs, i, n - 1, atlantic);
-        for (int j = 0; j < n; ++j) dfs(dfs, m - 1, j, atlantic);
+        for (int i = 0; i < m; ++i)
+            dfs(i, n - 1, atlantic, heights);
+        for (int j = 0; j < n; ++j)
+            dfs(m - 1, j, atlantic, heights);
 
         // Collect cells reachable by both
         vector<vector<int>> res;
