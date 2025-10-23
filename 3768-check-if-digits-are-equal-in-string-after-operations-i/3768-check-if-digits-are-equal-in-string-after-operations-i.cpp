c8e52dfa4aa69234a111cf4d12f12c34c658643c
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-10-23 22:48:47 +0530

    3768-CHECK-IF-DIGITS-ARE-EQUAL-IN-STRING-AFTER-OPERATIONS-I Stats: Time: 4 ms (75.59%), Space: 8.4 MB (95.79%) - LeetHub

---
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        if (s.size() == 2)
+            return s[0] - '0' == s[1] - '0';
+
+        while (s.size() > 2) {
+            for (int i = 0; i < s.size(); i++) {
+                s[i] = (char)(((s[i] - '0') + (s[i + 1] - '0')) % 10 + '0');
+                if (i == s.size() - 1)
+                    s.erase(i, i);
+            }
+        }
+
+        return s[0] - '0' == s[1] - '0';
+    }
+};
