a11d4b8495bacfcd671fcae681ba57c7700b18bb
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-10-25 23:35:32 +0530

    1817-CALCULATE-MONEY-IN-LEETCODE-BANK Stats: Time: 0 ms (100%), Space: 7.8 MB (53.24%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
@@ -1,28 +1,49 @@
 // T.C=O(N)
+// class Solution {
+// public:
+//     int weeklyStart = 1, money = 1;
+//     void solve(int currentDay, int previousMoney, int targetDay) {
+//         if (currentDay % 7 == 1) {
+//             weeklyStart++;
+//             money += weeklyStart;
+//             if (currentDay == targetDay) {
+//                 return;
+//             }
+//             solve(currentDay + 1, weeklyStart, targetDay);
+//         } else {
+//             previousMoney += 1;
+//             money += previousMoney;
+//             if (currentDay == targetDay) {
+//                 return;
+//             }
+//             solve(currentDay + 1, previousMoney, targetDay);
+//         }
+//     }
+//     int totalMoney(int n) {
+//         if (n == 1)
+//             return 1;
+//         solve(2, 1, n);
+//         return money;
+//     }
+// };
+
 class Solution {
 public:
-    int weeklyStart = 1, money = 1;
-    void solve(int currentDay, int previousMoney, int targetDay) {
-        if (currentDay % 7 == 1) {
-            weeklyStart++;
-            money += weeklyStart;
-            if (currentDay == targetDay) {
-                return;
-            }
-            solve(currentDay + 1, weeklyStart, targetDay);
-        } else {
-            previousMoney += 1;
-            money += previousMoney;
-            if (currentDay == targetDay) {
-                return;
-            }
-            solve(currentDay + 1, previousMoney, targetDay);
-        }
-    }
     int totalMoney(int n) {
         if (n == 1)
             return 1;
-        solve(2, 1, n);
+        int previousMoney = 1, currentDay = 2, weeklyStart = 1, money = 1;
+        while (currentDay <= n) {
+            if (currentDay % 7 == 1) {
+                weeklyStart++;
+                previousMoney = weeklyStart;
+                money += previousMoney;
+            } else {
+                previousMoney++;
+                money += previousMoney;
+            }
+            currentDay++;
+        }
         return money;
     }
 };
