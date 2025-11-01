06914f2ec918e6864152080a1a0258dabf33cc89
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-11-01 13:08:19 +0530

    3501-DELETE-NODES-FROM-LINKED-LIST-PRESENT-IN-ARRAY Stats: Time: 91 ms (82.16%), Space: 263.4 MB (41.15%) - LeetHub

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
@@ -0,0 +1,55 @@
+//No delete calls needed..leetcode does that stuff internally
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int> st(nums.begin(), nums.end());
+
+        while (head != NULL && st.find(head->val) != st.end()) {
+            //ListNode* temp = head;
+            head = head->next;
+            //delete (temp);
+        }
+
+        ListNode* dh = head;
+        while (dh != NULL && dh->next != NULL) {
+            if (st.find(dh->next->val) != st.end()) {
+                //ListNode* temp = dh->next;
+                dh->next = dh->next->next;
+                //delete (temp);
+            } else
+                dh = dh->next;
+        }
+
+        return head;
+    }
+};
+
+// T.C=O(NâM)=>T.L.E
+// class Solution {
+// public:
+//     ListNode* removeNode(int se, ListNode* head) {
+
+//         while (head && head->val == se) {
+//             head = head->next;
+//         }
+
+//         ListNode* dh = head;
+//         while (dh && dh->next != nullptr) {
+//             if (dh->next->val == se) {
+//                 dh->next = dh->next->next;
+//             } else
+//                 dh = dh->next;
+//         }
+
+//         return head;
+//     }
+
+//     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+//         for (int num : nums) {
+//             ListNode* modifiedHead = removeNode(num, head);
+//             head = modifiedHead;
+//         }
+
+//         return head;
+//     }
+// };
