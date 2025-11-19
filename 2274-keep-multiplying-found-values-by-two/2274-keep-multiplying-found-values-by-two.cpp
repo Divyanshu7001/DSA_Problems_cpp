cf8ee429c63eb761ea4e98502fe43a86e8777bb3
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-11-19 22:08:59 +0530

    2274-KEEP-MULTIPLYING-FOUND-VALUES-BY-TWO Stats: Time: 0 ms (100%), Space: 15.8 MB (20%) - LeetHub

---
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+
+        unordered_set<int> st(nums.begin(), nums.end());
+        while (true) {
+            if (st.find(original) != st.end())
+                original *= 2;
+            else break;
+        }
+        return original;
+    }
+};
