3ff447d2a622288f4ba9443d270b32d2d41d257a
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-12-05 22:11:10 +0530

    3704-COUNT-PARTITIONS-WITH-EVEN-SUM-DIFFERENCE Stats: Time: 12 ms (3.41%), Space: 23.2 MB (5.3%) - LeetHub

--- 3704-count-partitions-with-even-sum-difference/3704-count-partitions-with-even-sum-difference.cpp
+++ 3704-count-partitions-with-even-sum-difference/3704-count-partitions-with-even-sum-difference.cpp
@@ -1,12 +1,34 @@
+// T.C=O(n2)
+//  class Solution {
+//  public:
+//      int countPartitions(vector<int>& nums) {
+//          int res = 0;
+//          for (int i = 0; i < nums.size() - 1; i++) {
+//              int part1 = accumulate(nums.begin(), nums.begin() + i, 0);
+//              int part2 = accumulate(nums.begin() + i, nums.end(), 0);
+
+//             if (abs(part1 - part2) % 2 == 0)
+//                 res += 1;
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     int countPartitions(vector<int>& nums) {
-        int res = 0;
-        for (int i = 0; i < nums.size() - 1; i++) {
-            int part1 = accumulate(nums.begin(), nums.begin() + i, 0);
-            int part2 = accumulate(nums.begin() + i, nums.end(), 0);
 
-            if (abs(part1 - part2) % 2 == 0)
+        int n = nums.size();
+        int res = 0;
+        vector<int> prefSum(n, 0);
+        prefSum[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefSum[i] = prefSum[i - 1] + nums[i];
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            cout << "in loop " << i;
+            if (abs(prefSum[i] - (prefSum[n - 1] - prefSum[i])) % 2 == 0)
                 res += 1;
         }
         return res;
