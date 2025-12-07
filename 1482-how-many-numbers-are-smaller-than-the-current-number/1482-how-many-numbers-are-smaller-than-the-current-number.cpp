68c2bb857461727a8a855d8f8a54903ad3618e2b
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-12-07 23:54:54 +0530

    1482-HOW-MANY-NUMBERS-ARE-SMALLER-THAN-THE-CURRENT-NUMBER Stats: Time: 6 ms (51.13%), Space: 14.1 MB (79.04%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -5,25 +5,11 @@ public:
         int n=nums.size();
         for(int i=0;i<n;i++){
             int target=nums[i],c=0;
-            for(int j=0;j<n;j++){
+            for(int j=0;j<n;j++)
                 if(nums[j]<target)c++;
-            }
+            
             res.push_back(c);
         }        
         return res;
     }
 };
-// class Solution {
-// public:
-//     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-//         set<int, greater<int>> s(nums.begin(),nums.end());
-
-//         vector<int>res;
-//         int i=1;
-//         for(int n:s){
-//             res.push_back(s.size()-i);
-//             i++;
-//         }
-//         return res;
-//     }
-// };
