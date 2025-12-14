93d92ac257b683adcf453b5da3cdb06e2cfd34f6
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-12-14 13:12:53 +0530

    2251-NUMBER-OF-WAYS-TO-DIVIDE-A-LONG-CORRIDOR Stats: Time: 23 ms (70.5%), Space: 33.7 MB (81.5%) - LeetHub

--- 2251-number-of-ways-to-divide-a-long-corridor/2251-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2251-number-of-ways-to-divide-a-long-corridor/2251-number-of-ways-to-divide-a-long-corridor.cpp
@@ -2,14 +2,7 @@ class Solution {
 public:
     int MOD = 1e9 + 7;
     int numberOfWays(string corridor) {
-        int n = corridor.length();
-        if (n == 1)
-            return 0;
-        if (n == 2) {
-            if (corridor[0] == 'S' && corridor[1] == 'S')
-                return 1;
-            return 0;
-        }
+       
 
         int count = 0, prefix = 0, ans = 1;
         for (char c : corridor) {
