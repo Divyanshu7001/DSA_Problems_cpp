42ef0bc62272db0daa06d332a267590ed8a535b1
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-12-25 12:46:54 +0530

    3351-MAXIMIZE-HAPPINESS-OF-SELECTED-CHILDREN Stats: Time: 92 ms (98.23%), Space: 107.9 MB (47.16%) - LeetHub

--- 3351-maximize-happiness-of-selected-children/3351-maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/3351-maximize-happiness-of-selected-children.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
-
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
         sort(happiness.begin(), happiness.end(), greater<int>());
-        long long res = happiness[0];
+        long long res = (long long)happiness[0];
 
         for (int i = 1; i < k; i++) {
             int current = happiness[i] - i;
