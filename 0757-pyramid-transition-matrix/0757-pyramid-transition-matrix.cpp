74e6f2699f15f062e1b812544e08f6ecf6993258
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2025-12-29 23:56:36 +0530

    0757-PYRAMID-TRANSITION-MATRIX Stats: Time: 1013 ms (30.27%), Space: 157.8 MB (5.41%) - LeetHub

--- 0757-pyramid-transition-matrix/0757-pyramid-transition-matrix.cpp
+++ 0757-pyramid-transition-matrix/0757-pyramid-transition-matrix.cpp
@@ -1,10 +1,9 @@
 // CLassic Backtracking Problem example
 // Multiple choices and paths possible & we need to find one possible solution
 class Solution {
-public:
+    unordered_map<string, vector<char>> mp;
     unordered_map<string, bool> t;
-    bool solve(string curr, unordered_map<string, vector<char>>& mp, int idx,
-               string above) {
+    bool solve(string curr, int idx, string above) {
 
         if (curr.length() == 1)
             return true;
@@ -15,7 +14,7 @@ public:
             return t[key];
 
         if (idx == curr.length() - 1)
-            return t[key] = solve(above, mp, 0, "");
+            return t[key] = solve(above, 0, "");
         // reached end index of current  string,so calling
         // the solve again with above as current for that &
         // empty string as above for that
@@ -28,7 +27,7 @@ public:
         for (char& ch : mp[pair]) {
             above.push_back(ch); // attached with above (DO)
 
-            if (solve(curr, mp, idx + 1, above) == true)
+            if (solve(curr, idx + 1, above) == true)
                 return t[key] = true; // EXPLORE
 
             above.pop_back(); // UNDO
@@ -37,14 +36,12 @@ public:
         return t[key] = false;
     }
 
+public:
     bool pyramidTransition(string bottom, vector<string>& allowed) {
-
-        unordered_map<string, vector<char>> mp;
-
         for (auto& pattern : allowed) {
             mp[pattern.substr(0, 2)].push_back(pattern[2]); // ab->{c}
         }
 
-        return solve(bottom, mp, 0, "");
+        return solve(bottom, 0, "");
     }
 };
