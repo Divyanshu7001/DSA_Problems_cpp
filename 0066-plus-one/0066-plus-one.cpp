957e26a6f2cbc810b5989c679207f8ca687ea6ce
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2026-01-01 23:49:10 +0530

    0066-PLUS-ONE Stats: Time: 0 ms (100%), Space: 11.7 MB (14.65%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -3,12 +3,16 @@ public:
     vector<int> plusOne(vector<int>& digits) {
 
         for (int i = digits.size() - 1; i >= 0; i--) {
+            
             if (digits[i] != 9) {
+                //no carry left situation
                 digits[i] += 1;
                 return digits;
             }
+            //this digit was a 9..so we update this index to zero and loop continues
             digits[i] = 0;
         }
+        //This means all the digits were 9
         digits.insert(digits.begin(), 1);
         return digits;
     }
