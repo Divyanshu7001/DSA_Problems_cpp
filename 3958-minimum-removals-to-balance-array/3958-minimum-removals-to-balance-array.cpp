25e7a52db386ccf9bf01fa531d2d21fb81e73cf5
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2026-02-06 12:34:16 +0530

    3958-MINIMUM-REMOVALS-TO-BALANCE-ARRAY Stats: Time: 44 ms (24.42%), Space: 105.1 MB (52.91%) - LeetHub

--- 3958-minimum-removals-to-balance-array/3958-minimum-removals-to-balance-array.cpp
+++ 3958-minimum-removals-to-balance-array/3958-minimum-removals-to-balance-array.cpp
@@ -5,17 +5,15 @@ public:
         if (n == 1)
             return 0;
 
-        int i = 0, j = 0;
-
+        int i = 0, j = 0, maxBalance = 0;
         sort(nums.begin(), nums.end());
-        int maxBalance = 0;
+
         while (i < n && j < n) {
             if ((long)nums[j] <= (long)nums[i] * k) {
                 maxBalance = max(maxBalance, (j - i + 1));
                 j++;
-            } else if (nums[j] > nums[i] * k) {
+            } else if (nums[j] > nums[i] * k)
                 i++;
-            }
         }
         return n - maxBalance;
     }
