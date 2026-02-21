93880950199be8ec23b0da1611153310d454607b
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2026-02-21 15:50:14 +0530

    0767-PRIME-NUMBER-OF-SET-BITS-IN-BINARY-REPRESENTATION Stats: Time: 2 ms (77.88%), Space: 7.9 MB (88.15%) - LeetHub

--- 0767-prime-number-of-set-bits-in-binary-representation/0767-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0767-prime-number-of-set-bits-in-binary-representation/0767-prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,29 +1,53 @@
 class Solution {
 public:
-    bool isPrimeSetBit(string num) {
-        int setBit = 0;
-        for (char& str : num)
-            setBit += str == '1' ? 1 : 0;
-
-        if (setBit == 0 || setBit == 1)
+    bool isPrime(int n){
+        if(n==1){
             return false;
-        if (setBit == 2)
-            return true;
-
-        for (int i = 2; i * i <= setBit; i++) {
-            if (setBit % i == 0)
+        }
+        for(int i = 2;i<=sqrt(n);i++){
+            if(n%i==0){
                 return false;
+            }
         }
         return true;
     }
     int countPrimeSetBits(int left, int right) {
-        int res = 0;
-        for (int i = left; i <= right; i++) {
-            string num = bitset<32>(i).to_string().substr(
-                bitset<32>(i).to_string().find('1'));
-            if (isPrimeSetBit(num))
-                res++;
+        int ans = 0;
+        for(int i = left;i<=right;i++){
+            if(isPrime(__builtin_popcount(i))){
+                ans++;
+            }
         }
-        return res;
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+//     bool isPrimeSetBit(string num) {
+//         int setBit = 0;
+//         for (char& str : num)
+//             setBit += str == '1' ? 1 : 0;
+
+//         if (setBit == 0 || setBit == 1)
+//             return false;
+//         if (setBit == 2)
+//             return true;
+
+//         for (int i = 2; i <= sqrt(setBit); i++) {
+//             if (setBit % i == 0)
+//                 return false;
+//         }
+//         return true;
+//     }
+//     int countPrimeSetBits(int left, int right) {
+//         int res = 0;
+//         for (int i = left; i <= right; i++) {
+//             string num = bitset<32>(i).to_string().substr(
+//                 bitset<32>(i).to_string().find('1'));
+//             if (isPrimeSetBit(num))
+//                 res++;
+//         }
+//         return res;
+//     }
+// };
