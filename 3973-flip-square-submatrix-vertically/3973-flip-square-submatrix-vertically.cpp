30b90336f9c86f22acb67857946960df1ca360c7
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2026-03-22 00:44:15 +0530

    3973-FLIP-SQUARE-SUBMATRIX-VERTICALLY Stats: Time: 0 ms (100%), Space: 33.9 MB (60.84%) - LeetHub

--- 3973-flip-square-submatrix-vertically/3973-flip-square-submatrix-vertically.cpp
+++ 3973-flip-square-submatrix-vertically/3973-flip-square-submatrix-vertically.cpp
@@ -5,7 +5,7 @@ public:
 
         int row = x + k - 1;
         for (int i = x; i <= grid.size() - 1; i++) {
-            if (row == 0 || i > row)
+            if (i > row)
                 break;
             for (int j = y; j < y + k; j++) {
                 swap(grid[i][j], grid[row][j]);
