a60105dd94d71fbe885a8705daa5735244cd84da
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2026-03-29 19:19:20 +0530

    2999-CHECK-IF-STRINGS-CAN-BE-MADE-EQUAL-WITH-OPERATIONS-I Stats: Time: 0 ms (100%), Space: 8.8 MB (29.03%) - LeetHub

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/2999-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/2999-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,36 +1,33 @@
-class Solution {
-public:
-    bool canBeEqual(string s1, string s2) {
-
-        bool c1 = ((s1[0] == s2[0] && s1[2] == s2[2]) ||
-                   (s1[0] == s2[2] && s1[2] == s2[0]));
-        bool c2 = ((s1[1] == s2[1] && s1[3] == s2[3]) ||
-                   (s1[1] == s2[3] && s1[3] == s2[1]));
-
-    
-        return c1 && c2;
-    }
-};
 // class Solution {
 // public:
 //     bool canBeEqual(string s1, string s2) {
-//         unordered_set<char> st;
-//         for (int i : {0, 2}) {
-//             st.insert(s1[i]);
-//             st.insert(s2[i]);
-//         }
 
-//         if (st.size() > 2)
-//             return false;
-//         st.clear();
+//         bool c1 = ((s1[0] == s2[0] && s1[2] == s2[2]) ||
+//                    (s1[0] == s2[2] && s1[2] == s2[0]));
+//         bool c2 = ((s1[1] == s2[1] && s1[3] == s2[3]) ||
+//                    (s1[1] == s2[3] && s1[3] == s2[1]));
 
-//         for (int i : {1, 3}) {
-//             st.insert(s1[i]);
-//             st.insert(s2[i]);
-//         }
-//         if (st.size() > 2)
-//             return false;
-
-//         return true;
+//         return c1 && c2;
 //     }
 // };
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        int even[26] = {0}, odd[26] = {0};
+
+        for (int i = 0; i < 4; i++) {
+            if (i%2 == 0) {
+                even[s1[i] - 'a']++;
+                even[s2[i] - 'a']--;
+            } else {
+                odd[s1[i] - 'a']++;
+                odd[s2[i] - 'a']--;
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            if (even[i] != 0 || odd[i] != 0)
+                return false;
+        }
+        return true;
+    }
+};
