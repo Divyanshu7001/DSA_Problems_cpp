c2fa5968472319e239bbad7154e3d7280f0d8ea0
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2026-04-11 01:45:39 +0530

    4115-MINIMUM-DISTANCE-BETWEEN-THREE-EQUAL-ELEMENTS-I Stats: Time: 2 ms (75.88%), Space: 31.7 MB (5.2%) - LeetHub

--- 4115-minimum-distance-between-three-equal-elements-i/4115-minimum-distance-between-three-equal-elements-i.cpp
+++ 4115-minimum-distance-between-three-equal-elements-i/4115-minimum-distance-between-three-equal-elements-i.cpp
@@ -19,12 +19,12 @@ public:
         int n = nums.size();
         if (n < 3)
             return -1;
-        if (n == 3) {
-            if (nums[0] != nums[1] || nums[1] != nums[2])
-                return -1;
-            else
-                return 4;
-        }
+        // if (n == 3) {
+        //     if (nums[0] != nums[1] || nums[1] != nums[2])
+        //         return -1;
+        //     else
+        //         return 4;
+        // }
 
         unordered_map<int, vector<int>> mp;
         for (int i = 0; i < n; i++) {
