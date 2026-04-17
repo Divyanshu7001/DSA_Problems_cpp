e5d52e38a6c9a305c3655bfdedd5209d61affdf2
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2026-04-17 17:18:14 +0530

    4139-MINIMUM-ABSOLUTE-DISTANCE-BETWEEN-MIRROR-PAIRS Stats: Time: 115 ms (60.78%), Space: 131.7 MB (99.02%) - LeetHub

--- 4139-minimum-absolute-distance-between-mirror-pairs/4139-minimum-absolute-distance-between-mirror-pairs.cpp
+++ 4139-minimum-absolute-distance-between-mirror-pairs/4139-minimum-absolute-distance-between-mirror-pairs.cpp
@@ -1,3 +1,5 @@
+// Storing the reverse of numbers and iterating to find the reverse stored
+// number as present in the original arrray
 class Solution {
 public:
     int reverse(int num) {
@@ -14,23 +16,54 @@ public:
 
     int minMirrorPairDistance(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, vector<int>> mp;
-
-        for (int i = 0; i < n; i++)
-            mp[nums[i]].push_back(i);
-
+        unordered_map<int, int> mp;
         int res = INT_MAX;
         for (int i = 0; i < n; i++) {
-            int rev = reverse(nums[i]);
-            if (!mp.count(rev))
-                continue;
+            if (mp.count(nums[i]))
+                res=min(res,i-mp[nums[i]]);
 
-            vector<int>& indexList = mp[rev];
-            auto it = upper_bound(indexList.begin(), indexList.end(), i);
-            if (it != indexList.end())
-                res = min(res, *it - i);
+            mp[reverse(nums[i])]=i;
         }
 
         return res == INT_MAX ? -1 : res;
     }
 };
+
+// Storing the index of numbers...then iterating and using upper bound(bin
+// search) for find and minimize
+//  class Solution {
+//  public:
+//      int reverse(int num) {
+//          int res = 0;
+//          if (num < 10)
+//              return num;
+//          while (num > 0) {
+//              int dig = num % 10;
+//              res = (res * 10) + dig;
+//              num /= 10;
+//          }
+//          return res;
+//      }
+
+//     int minMirrorPairDistance(vector<int>& nums) {
+//         int n = nums.size();
+//         unordered_map<int, vector<int>> mp;
+
+//         for (int i = 0; i < n; i++)
+//             mp[nums[i]].push_back(i);
+
+//         int res = INT_MAX;
+//         for (int i = 0; i < n; i++) {
+//             int rev = reverse(nums[i]);
+//             if (!mp.count(rev))
+//                 continue;
+
+//             vector<int>& indexList = mp[rev];
+//             auto it = upper_bound(indexList.begin(), indexList.end(), i);
+//             if (it != indexList.end())
+//                 res = min(res, *it - i);
+//         }
+
+//         return res == INT_MAX ? -1 : res;
+//     }
+// };
