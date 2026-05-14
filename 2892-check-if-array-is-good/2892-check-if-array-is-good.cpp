b144866fc3632d719d7025d6a69d27cd2c653cad
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2026-05-15 02:50:27 +0530

    2892-CHECK-IF-ARRAY-IS-GOOD Stats: Time: 0 ms (100%), Space: 27 MB (47.16%) - LeetHub

--- 2892-check-if-array-is-good/2892-check-if-array-is-good.cpp
+++ 2892-check-if-array-is-good/2892-check-if-array-is-good.cpp
@@ -1,34 +1,57 @@
 class Solution {
 public:
-    int n, max;
-    bool isIncreasing(vector<int>& nums) {
-        int count = 0;
-        for (int i = 1; i < n; i++) {
-            if (nums[i] - nums[i - 1] != 1 && nums[i] != max)
-                return false;
-            else if (nums[i] == max)
-                count++;
-        }
-
-        if (count != 2)
-            return false;
-
-        return true;
-    }
-
     bool isGood(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        if (nums[0] != 1)
-            return false;
-        max = *max_element(nums.begin(), nums.end());
-
-        n = nums.size();
+        int max = *max_element(nums.begin(), nums.end()), n = nums.size();
         if (n != max + 1)
             return false;
 
         if (n == 2)
             return nums[1] == nums[0];
 
-        return isIncreasing(nums);
+        vector<int> vec(max, 0);
+
+        for (int i = 0; i < n; i++)
+            vec[nums[i] - 1]++;
+
+        for (int i = 0; i < max - 1; i++)
+            if (vec[i] == 0)
+                return false;
+
+        return vec[max - 1] == 2;
     }
 };
+
+// class Solution {
+// public:
+//     int n, max;
+//     bool isIncreasing(vector<int>& nums) {
+//         int count = 0;
+//         for (int i = 1; i < n; i++) {
+//             if (nums[i] - nums[i - 1] != 1 && nums[i] != max)
+//                 return false;
+//             else if (nums[i] == max)
+//                 count++;
+//         }
+
+//         if (count != 2)
+//             return false;
+
+//         return true;
+//     }
+
+//     bool isGood(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         if (nums[0] != 1)
+//             return false;
+//         max = *max_element(nums.begin(), nums.end());
+
+//         n = nums.size();
+//         if (n != max + 1)
+//             return false;
+
+//         if (n == 2)
+//             return nums[1] == nums[0];
+
+//         return isIncreasing(nums);
+//     }
+// };
