ca469f4b0aef4719643fe6373229927aecf26a13
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2026-05-29 16:39:09 +0530

    3606-MINIMUM-ELEMENT-AFTER-REPLACEMENT-WITH-DIGIT-SUM Stats: Time: 0 ms (100%), Space: 27.8 MB (23.8%) - LeetHub

--- 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.cpp
+++ 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.cpp
@@ -3,13 +3,29 @@ public:
     int minElement(vector<int>& nums) {
         int minimum = INT_MAX;
         for (int& num : nums) {
-            string word = to_string(num);
             int res = 0;
-            for (char& ch : word) {
-                res += ch - '0';
+            while (num > 0) {
+                res += num % 10;
+                num /= 10;
             }
             minimum = min(minimum, res);
         }
         return minimum;
     }
 };
+
+// class Solution {
+// public:
+//     int minElement(vector<int>& nums) {
+//         int minimum = INT_MAX;
+//         for (int& num : nums) {
+//             string word = to_string(num);
+//             int res = 0;
+//             for (char& ch : word) {
+//                 res += ch - '0';
+//             }
+//             minimum = min(minimum, res);
+//         }
+//         return minimum;
+//     }
+// };
