5e28aa9134563aac1ffca6a9ab3055a0360a5edd
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2026-05-31 21:27:14 +0530

    2245-DESTROYING-ASTEROIDS Stats: Time: 28 ms (76.93%), Space: 106.7 MB (48.03%) - LeetHub

---
+++ 2245-destroying-asteroids/2245-destroying-asteroids.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(), asteroids.end());
+        long newMass=mass;
+        for (int& ast : asteroids) {
+            if (newMass < ast)
+                return false;
+            else
+                newMass = newMass + ast;
+        }
+
+        return true;
+    }
+};
