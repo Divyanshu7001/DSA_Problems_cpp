a3a3f8e9556ee35b51891d494538b94adfd77c11
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2026-06-17 01:29:46 +0530

    3931-PROCESS-STRING-WITH-SPECIAL-OPERATIONS-I Stats: Time: 3 ms (76.81%), Space: 39.7 MB (69.58%) - LeetHub

--- 3931-process-string-with-special-operations-i/3931-process-string-with-special-operations-i.cpp
+++ 3931-process-string-with-special-operations-i/3931-process-string-with-special-operations-i.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     string processStr(string s) {
-
         string res = "";
         for (int i = 0; i < s.length(); i++) {
             char ch = s[i];
-            if (ch == '*') {
+            if (ch >= 'a' && ch <= 'z')
+                res += ch;
+            else if (ch == '*') {
                 if (!res.empty())
                     res.pop_back();
             } else if (ch == '#') {
-                res.append(res);
+                res += res;
             } else if (ch == '%') {
                 reverse(res.begin(), res.end());
-            } else
-                res += ch;
+            }
         }
-
         return res;
     }
 };
