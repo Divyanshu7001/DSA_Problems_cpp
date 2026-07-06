a788e77c4ded4866f771491c20bbf21499d26a44
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2026-07-06 06:37:07 +0530

    1222-REMOVE-COVERED-INTERVALS Stats: Time: 0 ms (100%), Space: 15 MB (82.12%) - LeetHub

--- 1222-remove-covered-intervals/1222-remove-covered-intervals.cpp
+++ 1222-remove-covered-intervals/1222-remove-covered-intervals.cpp
@@ -1,3 +1,13 @@
+// Intuition
+
+// The key observation is that an interval can only be covered by another interval that starts before (or at the same point) and ends after (or at the same point).
+// To make this easy to detect, we first sort the intervals:
+// Increasing order of start point so that any potential covering interval appears before the intervals it may cover.
+// If two intervals have the same start, we sort them by decreasing end point. This ensures that the longer interval comes first, allowing the shorter one to be recognized as covered.
+// While traversing the sorted intervals, we keep track of the farthest end point (maxEnd) seen so far.
+// If the current interval ends before or at maxEnd, it is already covered by a previous interval.
+// Otherwise, it is not covered, so we count it and update maxEnd.
+// This way, every interval is processed only once after sorting, giving an efficient solution
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
