bb932d861b02da4c1c5ff68a95c0da9bba32212a
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2026-07-24 13:32:08 +0530

    3820-NUMBER-OF-UNIQUE-XOR-TRIPLETS-II Stats: Time: 589 ms (67.98%), Space: 35.3 MB (81.46%) - LeetHub

--- 3820-number-of-unique-xor-triplets-ii/3820-number-of-unique-xor-triplets-ii.cpp
+++ 3820-number-of-unique-xor-triplets-ii/3820-number-of-unique-xor-triplets-ii.cpp
@@ -1,24 +1,58 @@
+// With set..a bit more space complexity
+//  class Solution {
+//  public:
+//      int uniqueXorTriplets(vector<int>& nums) {
+
+//         int n = nums.size();
+
+//         // store all pair XORs
+//         unordered_set<int> st;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i; j < n; j++)
+//                 st.insert(nums[i] ^ nums[j]);
+//         }
+
+//         // Find triplets
+//         unordered_set<int> s2;
+//         for (int pairXor : st) {
+//             for (int& num : nums) {
+//                 s2.insert(pairXor ^ num);
+//             }
+//         }
+
+//         return s2.size();
+//     }
+// };
+
+// with vector..space:O(maxElement)
+
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
 
         int n = nums.size();
+        int maxEl = *max_element(nums.begin(), nums.end());
+        int T = 1;
+        while (T <= maxEl)
+            T <<= 1;
 
         // store all pair XORs
-        unordered_set<int> st;
+        vector<bool> s1(T, false);
+        vector<bool> s2(T, false);
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++)
-                st.insert(nums[i] ^ nums[j]);
+                s1[nums[i] ^ nums[j]] = true;
         }
 
         // Find triplets
-        unordered_set<int> s2;
-        for (int pairXor : st) {
+        for (int i = 0; i < s1.size(); i++) {
+            if (s1[i] == false)
+                continue;
             for (int& num : nums) {
-                s2.insert(pairXor ^ num);
+                s2[i ^ num] = true;
             }
         }
 
-        return s2.size();
+        return count(s2.begin(), s2.end(), true);
     }
 };
