c23b87de330bf28317d6574689704d7d2d6cd2df
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2026-07-28 23:33:53 +0530

    3812-SMALLEST-PALINDROMIC-REARRANGEMENT-I Stats: Time: 100 ms (28.79%), Space: 54.1 MB (95.38%) - LeetHub

--- 3812-smallest-palindromic-rearrangement-i/3812-smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/3812-smallest-palindromic-rearrangement-i.cpp
@@ -5,48 +5,14 @@ public:
         if (n == 1)
             return s;
 
-        vector<int> freq(26, 0);
-
-        for (int i = 0; i < n; i++)
-            freq[s[i] - 'a']++;
-
-        int halfSize = n / 2;
-
-        sort(s.begin(), s.begin() + halfSize);
+        sort(s.begin(), s.begin() + (n / 2));
+        
         int i = 0;
-        while (halfSize < n) {
+        while (n > (n / 2)) {
             s[n - 1] = s[i];
             i++;
             n--;
         }
-
-        // string res = "";
-        // for (int i = 0; i < 26; i++) {
-        //     if (freq[i] != 0) {
-        //         int toAdd = (freq[i] == 1) ? 1 : (freq[i] / 2);
-        //         if (res.length() < halfSize) {
-        //             while (toAdd) {
-        //                 res += i + 'a';
-        //                 toAdd--;
-        //                 freq[i]--;
-        //             }
-        //         } else
-        //             break;
-        //     }
-        // }
-
-        // for (int i = 25; i >= 0; i--) {
-        //     if (freq[i] != 0) {
-        //         if (res.length() != n) {
-        //             while (freq[i]!=0) {
-        //                 res += i + 'a';
-        //                 freq[i]--;
-        //             }
-        //         } else
-        //             break;
-        //     }
-        // }
-
         return s;
     }
 };
