c702b5ed3e0ecc4c654171fb79f759a586d66b6f
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2026-07-31 04:26:37 +0530

    0300-LONGEST-INCREASING-SUBSEQUENCE Stats: Time: 75 ms (49.9%), Space: 14.4 MB (61.9%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,27 +1,47 @@
+// class Solution {
+// public:
+//     int n;
+//     vector<vector<int>> dp;
+//     int solve(int idx, int P, vector<int>& nums) {
+//         if (idx >= n)
+//             return 0;
+
+//         if (P != -1 && dp[idx][P] != -1)
+//             return dp[idx][P];
+
+//         int take = 0, skip = 0;
+//         if (P == -1 || nums[idx] > nums[P])
+//             take = 1 + solve(idx + 1, idx, nums);
+
+//         skip = solve(idx + 1, P, nums);
+
+//         return P != -1 ? (dp[idx][P] = max(take, skip)) : max(take, skip);
+//     }
+
+//     int lengthOfLIS(vector<int>& nums) {
+//         n = nums.size();
+//         dp.resize(n + 1, vector<int>(n + 1, -1));
+
+//         return solve(0, -1, nums);
+//     }
+// };
+
 class Solution {
 public:
     int n;
-    vector<vector<int>> dp;
-    int solve(int idx, int P, vector<int>& nums) {
-        if (idx >= n)
-            return 0;
-
-        if (P != -1 && dp[idx][P] != -1)
-            return dp[idx][P];
-
-        int take = 0, skip = 0;
-        if (P == -1 || nums[idx] > nums[P])
-            take = 1 + solve(idx + 1, idx, nums);
-
-        skip = solve(idx + 1, P, nums);
-
-        return P != -1 ? (dp[idx][P] = max(take, skip)) : max(take, skip);
-    }
-
     int lengthOfLIS(vector<int>& nums) {
         n = nums.size();
-        dp.resize(n + 1, vector<int>(n + 1, -1));
+        vector<int> t(n + 1, 1);
+        int maxLIS = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    t[i] = max(t[i], t[j] + 1);
+                    maxLIS = max(maxLIS, t[i]);
+                }
+            }
+        }
 
-        return solve(0, -1, nums);
+        return maxLIS;
     }
 };
