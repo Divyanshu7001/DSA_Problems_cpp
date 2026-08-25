d3f30cbfd460f89a3bd137eb9e06f1035cc1fa24
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2026-08-25 12:45:46 +0530

    4080-SMALLEST-MISSING-MULTIPLE-OF-K Stats: Time: 2 ms (42.52%), Space: 25.1 MB (45.06%) - LeetHub

--- 4080-smallest-missing-multiple-of-k/4080-smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/4080-smallest-missing-multiple-of-k.cpp
@@ -3,14 +3,15 @@ public:
     int missingMultiple(vector<int>& nums, int k) {
 
         unordered_set<int> st(nums.begin(), nums.end());
-        int i = 1, res;
+        int i = 1;
         while (true) {
-            res = k * i;
-            i++;
-            if (!st.count(res))
+            k *= i;
+            if (!st.count(k))
                 break;
+            k/=i;
+            i++;
         }
 
-        return res;
+        return k;
     }
 };
