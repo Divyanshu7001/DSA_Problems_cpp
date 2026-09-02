3349f438bd94fb9d196295f1a5fd878227fad134
DIVYANSHU KR. VERMA <153366809+Divyanshu7001@users.noreply.github.com>
2026-09-02 12:44:30 +0530

    4256-CONSTRUCT-UNIFORM-PARITY-ARRAY-I Stats: Time: 0 ms (100%), Space: 30.1 MB (94.95%) - LeetHub

--- 4256-construct-uniform-parity-array-i/4256-construct-uniform-parity-array-i.cpp
+++ 4256-construct-uniform-parity-array-i/4256-construct-uniform-parity-array-i.cpp
@@ -1,79 +1,64 @@
+//Parity rule=> even-odd=odd & odd-even=odd && even-even=even && odd-odd=even;
+//so if all even or all odd=> true
+//if mixed odd and even-> then take one odd ..if present value is odd..ignore or if even..then subtract , going to odd anyway..so everything can be done odd.
 class Solution {
 public:
-    int n;
-    void solve(vector<int>& nums1, char ch, vector<bool>& res) {
-        for (int i = 0; i < n; i++) {
-            int val = nums1[i];
-            for (int j = 0; j < n; j++) {
-                if (nums1[j] == val)
-                    continue;
-                int diff = val - nums1[j];
-                if (diff < 0)
-                    diff *= -1;
-
-                if ((ch == 'O' && diff % 2 == 1) ||
-                    (ch == 'E' && diff % 2 == 0))
-                    res[diff - 1] = true;
-            }
-        }
-    }
-
     bool uniformArray(vector<int>& nums1) {
-        n = nums1.size();
-        if (n == 1)
-            return true;
-        vector<bool> allOdd(101, false);
-        vector<bool> allEven(101, false);
-
-        for (int& num : nums1) {
-            if (num % 2 == 1)
-                allOdd[num - 1] = true;
-            allEven[num - 1] = true;
-        }
-        int countE = count(allOdd.begin(), allOdd.end(), true);
-        int countO = count(allEven.begin(), allEven.end(), true);
-        if (countE == n || countO == n)
-            return true;
-
-        solve(nums1, 'O', allOdd);
-        solve(nums1, 'E', allEven);
-
-        int oddC = 0, evenC = 0;
-        for (int i = 0; i < 10; i++) {
-            if (allOdd[i] == true)
-                oddC++;
-
-            if (allEven[i] == true)
-                evenC++;
-        }
-
-        return (oddC == n || evenC == n) ? true : false;
+        return true;
     }
 };
 
+
+//Brute force
 // class Solution {
 // public:
-//     bool uniformArray(vector<int>& nums1) {
+//     int n;
+//     void solve(vector<int>& nums1, char ch, vector<bool>& res) {
+//         for (int i = 0; i < n; i++) {
+//             int val = nums1[i];
+//             for (int j = 0; j < n; j++) {
+//                 if (nums1[j] == val)
+//                     continue;
+//                 int diff = val - nums1[j];
+//                 if (diff < 0)
+//                     diff *= -1;
 
-//         vector<int> freq(10, 0);
-//         for (int& num : nums1)
-//             freq[num - 1]++;
-
-//         for (int i = 0; i < 10; i++) {
-//             if (freq[i] != 0 && i % 2 == 1) {
-//                 int j = 0;
-//                 bool found = false;
-//                 while (j < 10) {
-//                     if (i == j)
-//                         continue;
-//                     if (abs(nums1[i] - nums1[j]) % 2 == 1)
-//                         found = true;
-//                 }
-//                 if (!found)
-//                     return false;
+//                 if ((ch == 'O' && diff % 2 == 1) ||
+//                     (ch == 'E' && diff % 2 == 0))
+//                     res[diff - 1] = true;
 //             }
 //         }
+//     }
 
-//         return true;
+//     bool uniformArray(vector<int>& nums1) {
+//         n = nums1.size();
+//         if (n == 1)
+//             return true;
+//         vector<bool> allOdd(101, false);
+//         vector<bool> allEven(101, false);
+
+//         for (int& num : nums1) {
+//             if (num % 2 == 1)
+//                 allOdd[num - 1] = true;
+//             allEven[num - 1] = true;
+//         }
+//         int countE = count(allOdd.begin(), allOdd.end(), true);
+//         int countO = count(allEven.begin(), allEven.end(), true);
+//         if (countE == n || countO == n)
+//             return true;
+
+//         solve(nums1, 'O', allOdd);
+//         solve(nums1, 'E', allEven);
+
+//         int oddC = 0, evenC = 0;
+//         for (int i = 0; i < 10; i++) {
+//             if (allOdd[i] == true)
+//                 oddC++;
+
+//             if (allEven[i] == true)
+//                 evenC++;
+//         }
+
+//         return (oddC == n || evenC == n) ? true : false;
 //     }
 // };
